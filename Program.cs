8780b9d4ae95da8d66bc1f3bb14b36f90c41a7c3
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-12-31 00:20:37 +0100

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppDapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
